2f58c6a9c1aaea333b3b307010ddf7a8d2e7830b
kaundinya <kaundinya.kautilya@gmail.com>
2024-10-08 18:36:41 +0530

    improve main site functionality

--- frontend/main/main.js
+++ frontend/main/main.js
@@ -8,13 +8,24 @@ addTaskForm.addEventListener("submit", (e) => {
 
   const taskText = taskInput.value;
   if (taskText.trim() === "") return;
-  const newTask = taskTemplate.content.cloneNode(true);
-  newTask.querySelector("p").textContent = taskText;
 
-  const deleteBtn = newTask.querySelector("#deleteTask");
+  const newTask = taskTemplate.content.cloneNode(true);
+  newTask.querySelector(".taskText").textContent = taskText;
+
+  const deleteBtn = newTask.querySelector(".deleteTask");
   deleteBtn.addEventListener("click", () => {
     tasksContainer.removeChild(deleteBtn.parentElement); // Remove the task from the tasks container
   });
+  const taskTextElement = newTask.querySelector(".taskText");
+  const check = newTask.querySelector(".checkbox");
+  check.addEventListener("change", () => {
+    if (check.checked) {
+      taskTextElement.style.textDecoration = "line-through";
+    } else {
+      taskTextElement.style.textDecoration = "none";
+    }
+  });
+
   tasksContainer.appendChild(newTask);
-  taskInput.value = "";
+  taskInput.value = ""; // Clear the input field
 });
